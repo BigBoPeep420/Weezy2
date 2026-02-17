c6caa33695beb0b6fe8ac61ac94cb5c0380c1061
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-02-17 03:34:22 -0600

    Added Credits modal

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# friendly-sniffle
+# Weezy2
 
-Template for website using Webpack
+A 'simple' single webpage that fetches weather data using
+the Visual Crossing API. Background changes based on current
+weather and time of day.
+
+Icons from https://www.pictogrammers.com
+Cloud and Grass Images from https://www.vecteezy.com
--- src/index.js
+++ src/index.js
@@ -4,12 +4,48 @@ import { Weezy } from "./modules/Weezy.js";
 import { weatherDisplay } from "./components/weatherDisplay/weatherDisplay.js";
 import { WeatherAnimationController } from "./components/weatherAnim/weatherAnim.js";
 import { emmet } from "emmet-elem";
+import { stringToElement } from "@/modules/utils/domParse.js";
+import iconClose from "@/assets/icons/close-thick.svg";
 
 const content = emmet(`div#content>div.appTitle>h1{Weezy}+div.rainEmitter`);
 content.appendChild(weatherDisplay());
 const footer = emmet(`footer>p{Copyright © 2026 Lane Robey}`);
 
-document.body.append(content, footer);
+const creditsModal = createCreditsModal();
+const creditsLink = emmet('button.creditsLink[type="button"]{Credits}');
+creditsLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  creditsModal.showPopover();
+  return;
+});
+footer.appendChild(creditsLink);
+
+document.body.append(content, footer, creditsModal);
 
 const animController = new WeatherAnimationController(content);
 const weezy = new Weezy();
+
+function createCreditsModal() {
+  const modal = emmet(`div.creditsModal[popover="auto"]`);
+  const title = emmet(`div.modalTitle>p.title{Asset Credits}`);
+  const closeBtn = emmet(`button.close[type="button"]`);
+  closeBtn.appendChild(stringToElement(iconClose, "svg"));
+  title.appendChild(closeBtn);
+  modal.append(
+    title,
+    emmet(
+      `div.credits>p{Icons from }>a[href="https://www.pictogrammers.com"]{Pictogrammers}^` +
+        `p{Clouds and Grass from }>a[href="https://www.vecteezy.com"]{Vecteezy}^`,
+    ),
+  );
+
+  modal.addEventListener("click", (e) => {
+    if (e.target.closest(".close")) {
+      e.preventDefault();
+      modal.hidePopover();
+      return;
+    }
+  });
+
+  return modal;
+}
